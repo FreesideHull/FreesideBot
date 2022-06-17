8a73fced71ebf7528a840dcd6f37a4ae8dcd5d8e
lgibson02 <newlawrence2498@gmail.com>
2022-06-17 14:56:21 +0100

    add rest api, some cleanup and eslint enforced style

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 # Freeside Discord Bot
-Discord bot as used on https://discord.freeside.co.uk. Current use is filtering
-the `#news` channel to only allow news articles (posts containing a working
+Discord bot as used on https://discord.freeside.co.uk. The bot currently does
+the following:
+
+Filters `#news` channel to only allow news articles (posts containing a working
 web link) to be posted. A thread is automatically created for each article to
-allow for discussion to take place. The thread title uses the news article's
+allow for discussion to take place. The thread title uses the article's
 title (the OpenGraph title, falling back to the main title if not found).
 
-Settings are passed through environment variables.
+Assists in monitoring Freeside server infrastructure. The bot runs a small REST
+API web server to easily allow servers to send message notifications through the
+bot.
 
-`DISCORD_TOKEN=` Token used to authenticate the bot. [Required]
+Bot settings are passed through environment variables.
 
-`MESSAGE_EXPIRE_TIME=` Time it takes for a public message (at a user) to be
-automatically deleted in miliseconds. Default is 2 minutes (120000ms).
+`DISCORD_TOKEN=` Token used to authenticate the bot. [Required]  
+`REST_API_SERVER_ENABLED=`Enable REST API server.
+[Default: true]  
+`REST_API_SERVER_HOST=` Host to open REST API server on.
+[Default: 0.0.0.0]  
+`REST_API_SERVER_PORT=` Port to open REST API server on.
+[Default: 8000]  
+`REST_API_SERVER_USERNAME=` Username required to authenticate to REST API. 
+[Optional]  
+`REST_API_SERVER_PASSWORD=` Password required to authenticate to REST API.
+[Optional]  
+`PUBLIC_REPLY_DECAY_TIME=` Time it takes for a public message (at a user) to be
+automatically deleted in ms.
+[Default: 120000ms (2mins) ]
 
-### Docker Usage:
+### Docker Install
 You will need a file to set environment variables in. Create one called `./env`,
  this will be ignored by Git.
 ```sh
@@ -22,7 +38,7 @@ docker build -t freesidebot .
 docker run -d --name freesidebot --env-file=./env freesidebot
 ```
 
-### Manual Usage:
+### Manual Install
 ```sh
 git clone https://github.com/FreesideHull/FreesideBot
 cd ./FreesideBot
@@ -30,3 +46,45 @@ npm install
 export DISCORD_TOKEN=<bot token goes here> # use set DISCORD_TOKEN= on Windows
 node bot.js
 ```
+
+### REST API
+`POST` request parameters are always in the form of a string to string key value
+map. Parameters can passed through the request body (either URL encoded format
+or JSON format) or be included in the URL query string. Responses are always in
+JSON format.
+
+Status code is set `200` when successful, `400` if request is invalid and `500`
+if the server encounters an error. A request that can't be fulfilled will
+always return an object with an `error` attribute containing an error message
+(e.g `{error: "Error message. Something went wrong!"}`).
+
+Authentication is done through HTTP Basic Authentication scheme. All endpoints
+require authentication. Both username and password must be set for auth to be
+enabled.
+***
+## `POST /send`
+Send a Discord text message into a specified text channel. 
+
+Since channel is specified by name and not ID, the bot will check through all
+guilds for a channel with the specified name and post into all channels that it
+finds.
+
+### `Parameters`
+
+| Name      | Summary                                               |
+|-----------|-------------------------------------------------------|
+| `channel` | Name of channel to post into (e.g `general`).         |
+| `message` | Text message to post (e.g. `@everyone Hello World!`). |
+
+### `Response`
+| Attribute | Summary                                   | Type       |
+|-----------|-------------------------------------------|------------|
+| `posted`  | URLs to all successfully posted messages. | `string[]` |
+***
+
+#### API Use Example:
+Use cURL to post `Hello World!` to `#testing`. Authenticate as user `bob` with
+password `pass123`\.  
+`> curl -d "channel=testing" -d "message=Hello World! -u bob:pass123
+http://localhost:8000/send`  
+`{"posted":["https://discord.com/channels/57123123..."]}`
--- bot.js
+++ bot.js
@@ -1,110 +1,87 @@
-const fs = require("fs");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const escape = require("discord-escape");
 const discord = require("discord.js");
+const express = require("express");
+const expressAuthBasic = require("express-basic-auth");
 require("console-stamp")(console); // add console timestamp and log level info
 
-const client = new discord.Client({
-    intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]
-});
 
-// time taken for tempoary public messages to expire and be deleted (ms).
-const MESSAGE_EXPIRE_TIME = process.env.MESSAGE_EXPIRE_TIME || 120000;
-
+// bot options (see README)
 if (!process.env.DISCORD_TOKEN) {
     console.error("Set required DISCORD_TOKEN environment variable.");
     process.exit(1);
 }
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN.trim();
+const REST_API_SERVER_ENABLED = (process.env.REST_API_SERVER_ENABLED ||
+    "true").trim().match("^(true|on|y|1)") != null;
+const REST_API_SERVER_HOST = (process.env.REST_API_SERVER_HOST ||
+    "0.0.0.0").trim();
+const REST_API_SERVER_PORT = process.env.REST_API_SERVER_PORT || 8000;
+const REST_API_SERVER_USERNAME = process.env.REST_API_SERVER_USERNAME;
+const REST_API_SERVER_PASSWORD = process.env.REST_API_SERVER_PASSWORD;
+const PUBLIC_REPLY_DECAY_TIME = process.env.PUBLIC_REPLY_DECAY_TIME || 120000;
 
-client.login(process.env.DISCORD_TOKEN.trim());
-
-client.once("ready", () => {
-    console.log("Bot is now running.");
-});
-
-client.on("messageCreate", message => {
-    // ignore bots
-    if (message.author.bot) return; 
-    // deal with server text channels only
-    if (!(message.channel instanceof discord.TextChannel)) return;
-    // #news channel
-    if (message.channel.name == "news") return moderateNewsMessage(message);
-});
-
-/**
-    Inform user with a text message. First tries to send message to the user's
-    DM channel. If this fails (user has it closed) then the message will be
-    sent in publicChannel. Exception is thrown forward if this fails still.
-    
-    Message sent in publicChannel will expire and be deleted after a set period
-    of time for cleanliness. See MESSAGE_EXPIRE_TIME.
-*/
-async function inform(user, publicChannel, content, tryingPublic = false) {
-    // channel to send in
-    const channel = (tryingPublic && publicChannel) || user.dmChannel ||
-        await user.createDM();
-    let message;
-    try {
-        message = await channel.send(user.toString() + "\n" + content);
-    } catch (e) {
-        // message already tried through dms, public channel now also failed
-        if (tryingPublic) throw e;
-
-        // sending dm to user failed, try posting in public channel
-        message = await inform(user, publicChannel, content, true);
-
-        // clean up public messages after time
-        await new Promise(resolve => setTimeout(resolve, MESSAGE_EXPIRE_TIME));
-        try {
-            await message.delete();
-        } catch {
-            console.warn(`Failed to automatically delete ${message.url}. ` +
-                "Message may have already been deleted manually.");
-        }
+// setup bot and rest api server
+botSetup(DISCORD_TOKEN).then(bot => {
+    if (REST_API_SERVER_ENABLED) {
+        return restApiSetup(bot, REST_API_SERVER_PORT,
+            REST_API_SERVER_HOST, REST_API_SERVER_USERNAME,
+            REST_API_SERVER_PASSWORD);
     }
-    return message;
-} 
+}).catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
 
-/*
-    Remove a user's message with a reason.
-*/
-async function removeMessage(message, reason) {
-    console.log("Removing message '%s' by %s with explaination '%s'.",
-        message.content, message.author.tag, reason);
-    await message.delete();
-    /*
-        Send message to author with their quoted message and why it was removed. 
+// BOT CODE...
 
-        > message content quoted (replace "\n" with "\n>"to continue quote over
-            multiple lines)
-        ** This message was removed ** (reason)
-    */
-    await inform(message.author, message.channel, 
-        "> " + escape(message.content).replace(/\n/g, "\n> ") + "\n" +
-        "**This message was removed.** " + reason);
+function botSetup (token) {
+    const bot = new discord.Client({
+        intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]
+    });
+
+    bot.on("messageCreate", m => {
+    // ignore own and other bots
+        if (m.author.bot) return;
+        // deal with server text channels only
+        if (!(m.channel instanceof discord.TextChannel)) return;
+        // #news channel
+        if (m.channel.name == "news") return handleNewsMessage(m);
+    });
+
+    bot.login(token);
+
+    return new Promise((resolve, reject) => {
+        bot.once("ready", () => {
+            console.log("Discord bot is now up and running.");
+            resolve(bot);
+        });
+        bot.once("error", e => reject(e));
+    });
 }
 
 /*
     Called every time a message is posted into news channel.
 */
-async function moderateNewsMessage(message) {
+async function handleNewsMessage (message) {
     /*
-        The news channel can only contain links to news stories. A hyperlink to
-        a page containing an 'og:title' meta tag is considered a news story. A
-        message posted which does not contain this will be rejected.
+        The news channel can only contain links to news stories. A hyperlink
+        to a page containing an 'og:title' meta tag is considered a news story.
+        A message posted which does not contain this will be rejected.
     */
     const match = message.content.match(/https?:\/\/[^ "]{2,}/);
     const title = match && await fetchNewsTitle(match[0], message.author);
+
     if (title) {
-        // message determined to be a news story
+    // message determined to be a news story
         console.log("Creating news discussion thread for %s named '%s'.",
             message.author.tag, title);
         await message.startThread({
             name: title.substr(0, 100) // keep under thread limit
         });
     } else {
-        // not a news story, remove message
+    // not a news story, remove message
         await removeMessage(message, "Please only post news articles. " +
             "Discussion on news stories should take place inside their " +
             "designated thread which will be created automatically.");
@@ -115,36 +92,184 @@ async function moderateNewsMessage(message) {
     Fetch title of a news article given its URL. This title will be used for
     creating a thread.
 */
-async function fetchNewsTitle(url) {
+async function fetchNewsTitle (url) {
     console.log("Attempting to fetch page %s for news title.", url);
-    let response;
-    try {
-        response = await axios.get(url, {
-            headers: {
-                /*
-                    Use Chrome / Windows 10 user agent as a precaution against
-                    bad news sites which may reject requests due to user agent.
-                */
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-                    "AppleWebKit/537.36 (KHTML, like Gecko)" +
-                    "Chrome/101.0.4951.54 Safari/537.36"
-            }
-        });
-    } catch(e) {
-        console.log("Request for %s failed. %s", url, e.message);
-        return null; // request fails
-    } 
+    const response = await axios.get(url, {
+        headers: {
+            /*
+                Use Chrome / Windows 10 user agent as a precaution against
+                bad news sites which may reject requests due to user agent.
+            */
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                "Chrome/102.0.5005.115 Safari/537.36"
+        }
+    }).catch(e => console.log("Request for %s failed. %s", url, e.message));
+    if (!response) return null; // request failed
+
     const $ = cheerio.load(response.data); // parse response HTML
-    
+
     // first try using opengraph title
     const metaOgTitle = $("meta[property=og:title],meta[name=og:title]");
     if (metaOgTitle.length != 0) return metaOgTitle.attr("content");
     console.log("Page %s contains no OpenGraph title tag.", url);
-    
+
     // no opengraph title tag, so use the main title
     const title = $("title");
     if (title.length != 0 && title.text().length != 0) return title.text();
     console.log("Page %s also contains no title tag. Title not found.", url);
 
     return null; // no title found
+}
+
+/**
+    Inform user with a text message. First tries to send message to the user's
+    DM channel. If this fails (user has it closed) then the message will be
+    sent in publicChannel. Exception is thrown forward if this fails still.
+
+    Message sent in publicChannel will expire and be deleted after a set period
+    of time for cleanliness. See PUBLIC_REPLY_DECAY_TIME.
+*/
+async function inform (user, message, publicChannel = null,
+    tryingPublic = false) {
+    // channel to send in
+    const channel = (tryingPublic && publicChannel) || user.dmChannel ||
+        await user.createDM();
+    try {
+        return await channel.send(user.toString() + "\n" + message);
+    } catch (e) {
+    // message already tried through dms, public channel now also failed
+        if (tryingPublic || !publicChannel) throw e; // pass on exception
+    }
+
+    // to get here: sending dm to user failed, try posting in public channel
+    const reply = await inform(user, message, publicChannel, true);
+
+    // clean up public messages after time
+    await new Promise(resolve => setTimeout(resolve, PUBLIC_REPLY_DECAY_TIME));
+    try {
+        await reply.delete();
+    } catch {
+        console.warn(`Failed to automatically delete ${message.url}. ` +
+            "Message may have already been deleted manually.");
+    }
+}
+
+/*
+    Remove a user's message with a reason.
+*/
+async function removeMessage (message, reason) {
+    console.log("Removing message '%s' by %s with explaination '%s'.",
+        message.content, message.author.tag, reason);
+    await message.delete();
+
+    /*
+        Send message to author with their quoted message and why it was removed.
+
+        > message content quoted (replace "\n" with "\n>"to continue quote over
+            multiple lines)
+        ** This message was removed ** (reason)
+    */
+    await inform(message.author,
+        "> " + escape(message.content).replace(/\n/g, "\n> ") + "\n" +
+        "**This message was removed.** " + reason, message.channel);
+}
+
+// REST API CODE...
+
+function restApiSetup (bot, port, host, username = null, password = null) {
+    const server = express();
+    const listener = server.listen(port, host);
+
+    // log requests
+    server.use((req, res, next) => {
+        let requestedWith = req.get("User-Agent") || "";
+        if (requestedWith) requestedWith = " with " + requestedWith;
+        console.log("%s - Requested %s %s://%s:%i%s%s.", req.ip, req.method,
+            req.protocol, req.hostname, listener.address().port, req.url,
+            requestedWith);
+        next();
+    });
+
+    // handle authentication if credential requirements are set
+    if (username && password) {
+        server.use(expressAuthBasic({
+            users: { [username]: password },
+            challenge: true,
+            unauthorizedResponse: req => (req.auth ? "Incorrect" : "Missing") +
+                " credentials."
+        }));
+    }
+
+    // allow handling url encoded or json encoded body
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+
+    // handle POST /send
+    server.post("/send", (req, res) => restApiSendMessage(bot, req, res));
+
+    // setup server listener
+    return new Promise((resolve, reject) => {
+        listener.once("listening", () => {
+            console.log("REST API server is listening on %s:%i.", host,
+                listener.address().port);
+            resolve(server);
+        });
+        listener.once("error", e => reject(e));
+    });
+}
+
+// handle POST /send requests
+async function restApiSendMessage (bot, req, res) {
+    // check channel parameter
+    const channelName = (req.body.channel || req.query.channel || "").trim()
+        .replace(/^#/, "").toLowerCase();
+    if (!channelName) {
+        res.status(400).send({ error: "Missing 'channel' parameter." });
+        return;
+    }
+
+    // check message parameter
+    const message = req.body.message || req.query.message;
+    if (!message) {
+        res.status(400).send({ error: "Missing 'message' parameter." });
+        return;
+    }
+
+    if (bot.guilds.cache.length == 0) {
+        res.status(500).send({ error: "Bot is not in any guilds." });
+        return;
+    }
+
+    // send out messages, posted will be an array of urls to messages which were
+    // successfully posted
+    const posted = await Promise.allSettled(
+    // for all guilds...
+        bot.guilds.cache.map(guild => {
+            // get text channel with name (specified in user request)
+            const channel = guild.channels.cache.find(
+                c => c.name == channelName && c instanceof discord.TextChannel
+            );
+            // if channel exists: send user message into it and return msg url
+            if (channel) {
+                return channel.send(message).then(m => m.url)
+                    .catch(console.error);
+            }
+            // return null / undefined for errors or if channel not found
+            return null;
+        })
+    // filter and map to get array of urls
+    ).then(results => results.filter(r => r.value).map(r => r.value));
+
+    if (posted.length == 0) {
+        res.status(500).send({
+            error: "No messages were posted successfully. " +
+            `Check text channel #${channelName} exists.`
+        });
+    } else {
+        console.log("%s - Posted message '%s' in #%s in %i guild(s). " +
+            "Message URLs: %s.", req.ip, req.body.message, channelName,
+        posted.length, posted.join(", "));
+        res.send({ posted });
+    }
 }
